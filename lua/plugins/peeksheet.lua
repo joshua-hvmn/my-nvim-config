80d0622aa7553ebb160001f39fb2875b3fdd8f1a
joshua-hvmn <secondjoshua1@gmail.com>
2026-06-18 19:29:05 -0700

    change: peeksheet init

--- lua/plugins/peeksheet.lua
+++ lua/plugins/peeksheet.lua
@@ -1,10 +1,11 @@
 return {
   'joshua-hvmn/peeksheet.nvim',
-  lazy = false,
-  config = function()
-    require('peeksheet').setup {}
-  end,
+  lazy = true,
+  cmd = 'Peeksheet',
   keys = {
     { '<leader>h', '<cmd>Peeksheet<CR>', desc = 'Open Peeksheet' },
   },
+  config = function()
+    require('peeksheet').setup {}
+  end,
 }
